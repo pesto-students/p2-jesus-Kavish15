34b8f57242f6f748bcf9739a4442bdd133347737
Kavish15 <kavishgarg15@gmail.com>
2022-06-01 00:07:54 +0530

    Adding Assignment 3.4

---
+++ Week-3/Exercise 3.4/assignment 4th.js
@@ -0,0 +1,20 @@
+
+function createStack() {
+    const items = []; // creating local variable, 
+    return {
+      push(item) {
+        items.push(item);
+      },
+      pop() {
+        return items.pop();
+      },
+    };
+  }
+  const stack = createStack();
+  stack.push(10);
+  stack.push(5);
+  console.log(stack.pop()); // => 5
+  console.log(stack.items); // => undefined
+  
+  
+  
